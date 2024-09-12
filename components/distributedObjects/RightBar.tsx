4eb24e381dd1e0bb1824cec64d84566a34102d2d
Daniel Azil <danielazil.fse@gmail.com>
2024-09-12 15:45:00 +0000

    Add RightSidebar component to display suggested communities and similar users

---
+++ components/distributedObjects/RightBar.tsx
@@ -0,0 +1,73 @@
+import { currentUser } from "@clerk/nextjs";
+
+import StyleUserCard from "../UIStyleCards/StyleUserCard";
+
+import { RetrieveGroup } from "@/lib/backend.imp/group.implementation";
+import { fetchUsers } from "@/lib/backend.imp/user.implementation";
+
+async function RightSidebar() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const similarMinds = await fetchUsers({
+    userId: user.id,
+    pageSize: 4,
+  });
+
+  const suggestedCOmmunities = await RetrieveGroup({ pageSize: 4 });
+
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>
+          Suggested Communities
+        </h3>
+
+        <div className='mt-7 flex w-[350px] flex-col gap-9'>
+          {suggestedCOmmunities.communities.length > 0 ? (
+            <>
+              {suggestedCOmmunities.communities.map((community) => (
+                <StyleUserCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  personType='Community'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>
+              No communities yet
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
+        <div className='mt-7 flex w-[350px] flex-col gap-10'>
+          {similarMinds.users.length > 0 ? (
+            <>
+              {similarMinds.users.map((person) => (
+                <StyleUserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType='User'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>No users yet</p>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default RightSidebar;
