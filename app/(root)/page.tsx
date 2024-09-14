d80f377919a06356ce05b3edf3417847768a5727
Daniel Azil <danielazil.fse@gmail.com>
2024-09-14 14:35:42 +0000

    Add RootLayout component with ClerkProvider and layout structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,61 @@
-import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+import PostCard from "@/components/UIStyleCards/PostCard";
+import PagePagination from "@/components/distributedObjects/PagePagination";
+
+import { fetchPosts } from "@/lib/backend.imp/post.implementation";
+import { retrieveUserQR } from "@/lib/backend.imp/user.implementation";
+
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await retrieveUserQR(user.id);
+  if (!userInfo?.onboarded) redirect("/setup");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
+
   return (
-    <div>
-      <UserButton afterSignOutUrl="/"/>
-    </div>
+    <>
+      <h1 className='head-text text-left'>Feeds</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <PostCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      <PagePagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
   );
 }
+
+export default Home;
