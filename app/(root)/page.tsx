5d15ead89911638e887ac05c3565ec40b14a2958
Daniel Azil <danielazil.fse@gmail.com>
2024-09-16 12:28:19 +0000

    Update home page with userbutton from clerk package.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main>
-      <h1>~tether</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   );
 }
